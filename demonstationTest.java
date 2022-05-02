b5ea4445c05d064483a4545160c1d938693a99b2
victorvm77 <vicgut770@gmail.com>
2022-05-01 23:45:22 -0700

    added junit

---
+++ demonstationTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class demonstationTest {
+    @Test
+    public void Mult(){
+        assertEquals(4, skill_demonstration.times(2, 2));
+    }
+}
--- skill_demonstration.java
+++ skill_demonstration.java
@@ -1,3 +1,6 @@
 public class skill_demonstration{
     //Add code 
+    public static int times(int x, int y){
+        return x * y;
+    }
 }
